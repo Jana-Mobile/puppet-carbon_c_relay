468be368e61801adfad5c15e83467c453486eb53
Andrew Choo <achoo@vonage.com>
2016-08-08 15:21:10 -0400

    Add vars for options found in newer versions of carbon-c-relay Added header Account for EL 6 Default sorted_matched to false worker_threads undef since daemon will auto detect Remove unused vars

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,48 +1,49 @@
+# == Class: carbon_c_relay::params
 #
 class carbon_c_relay::params {
   $allowed_chars               = undef
-  $carbon_cache_statistics     = false
   $config_clusters             = {}
   $config_file                 = '/etc/carbon-c-relay.conf'
   $config_matches              = {}
   $config_rewrites             = {}
   $group                       = 'carbon-c-relay'
-  $init_file                   = '/etc/init.d/carbon-c-relay'
-  $init_file_ensure            = 'file'
-  $init_template               = "carbon_c_relay${init_file}.erb"
-  $interface                   = 'all'
+  $interface                   = undef
   $io_timeout                  = 600
-  $limit_fsize                 = undef
-  $limit_cpu                   = undef
   $limit_as                    = undef
-  $limit_no_file               = 64000
+  $limit_cpu                   = undef
+  $limit_fsize                 = undef
+  $limit_nofile                = 64000
   $limit_nproc                 = 64000
+  $limits_file                 = '/etc/security/limits.d/carbon-c-relay.conf'
+  $limits_template             = "carbon_c_relay${limits_file}.erb"
   $listen                      = 2003
-  $listen_backlog              = 3
+  $listen_backlog              = 32
   $log_dir                     = '/var/log/carbon-c-relay'
   $log_file                    = 'carbon-c-relay.log'
   $max_stalls                  = 4
-  $output_file                 = undef
   $package_ensure              = latest
   $package_manage              = true
   $package_name                = 'carbon-c-relay'
   $pid_dir                     = '/var/run/carbon-c-relay'
   $pid_file                    = 'carbon-c-relay.pid'
-  $replication_factor          = 1
   $server_batch_size           = 2500
   $server_queue_size           = 25000
   $service_enable              = true
   $service_ensure              = running
-  $service_file                = '/etc/systemd/system/carbon-c-relay.service'
+  $service_file                = $::operatingsystemmajrelease ? {
+    '6' => '/etc/init.d/carbon-c-relay',
+    '7' => '/usr/lib/systemd/system/carbon-c-relay.service',
+  }
   $service_manage              = true
   $service_name                = $package_name
   $service_template            = "carbon_c_relay${service_file}.erb"
-  $sorted_matches              = true
+  $sorted_matches              = false
   $sorted_rewrites             = false
   $statistics_hostname         = undef
+  $statistics_non_cumulative   = false
   $statistics_sending_interval = 60
   $sysconfig_file              = '/etc/sysconfig/carbon-c-relay'
   $sysconfig_template          = "carbon_c_relay${sysconfig_file}.erb"
   $user                        = 'carbon-c-relay'
-  $worker_threads              = 4
+  $worker_threads              = undef
 }
